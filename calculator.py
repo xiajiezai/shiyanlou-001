60ee22c4abad44a59f1910fc64208529ad042a89
81623405 <81623405@qq.com>
2018-01-20 16:50:27 +0800

    TaxCalculator

--- calculator.py
+++ calculator.py
@@ -8,34 +8,36 @@ IncomeTaxQuickLookupItem = namedtuple(
 Threshold=3500
 QUICK_LOOKUP=[
 	IncomeTaxQuickLookupItem(80000,0.45,13505),
-	IncomeTaxQuickLookupItem(55000,.35,5505),
-	IncomeTaxQuickLookupItem(35000,.3,2755),
+	IncomeTaxQuickLookupItem(55000,0.35,5505),
+	IncomeTaxQuickLookupItem(35000,0.3,2755),
 	IncomeTaxQuickLookupItem(9000,0.25,1005),
-	IncomeTaxQuickLookupItem(4500,.2,555),
-	IncomeTaxQuickLookupItem(1500,.1,105),
+	IncomeTaxQuickLookupItem(4500,0.2,555),
+	IncomeTaxQuickLookupItem(1500,0.1,105),
 	IncomeTaxQuickLookupItem(0,0.03,0)
 ]
 
-def payable(EBT):
+def earningsafterinsurance(EBT):
 	EndowmentRate=.08
 	MedicalRate=.02
 	UnemploymentRate=.005
 	EmploymentInjuryRate=0
 	MaternityRate=0
 	HousingFundRate=0.06
-	TotalRate=EndowmentRate+MedicalRate+ UnemploymentRate + MaternityRate+\
-	HousingFundRate
-	Payable=EBT*(1-TotalRate)-Threshold
-	return Payable
+	TotalRate=EndowmentRate+MedicalRate+ UnemploymentRate + \
+	EmploymentInjuryRate + MaternityRate+ HousingFundRate
+	EarningsAfterInsurance=EBT*(1-TotalRate)
+	return EarningsAfterInsurance
 
 
-def EAT(Payable):
+def EAT(EarningsAfterInsurance):
+	Payable=EarningsAfterInsurance-Threshold
 	if Payable<=0:
-		return '0.00'
-		for item in QUICK_LOOKUP:
-			if Payable > item.TaxableBracket:
-				EAT = Payable* item.TaxRate- Subtractor
-				return ('{:.2f}'.format(EAT))
+		return EarningsAfterInsurance
+	for item in QUICK_LOOKUP:
+		if Payable > item.TaxableBracket:
+			TAX = Payable*item.TaxRate-item.Subtractor
+			EAT=EarningsAfterInsurance-TAX
+			return EAT
 
 
 
@@ -43,15 +45,14 @@ def main():
 	import sys
 	item=sys.argv[1:]
 	for argv in item:
-		EmployeeNumberAndEBT=argv.split(':')
-		EmployeeNumber=EmployeeNumberAndEBT[0]
+		EmployeeNumber, EBTString=argv.split(':')
 		try:
-			EBT=int(EmployeeNumberAndEBT[1])
+			EBT=int(EBTString)
 		except ValueError:
 			print('Parameter Error')
 			exit()
-		Salary=EAT(payable(EBT))
-		print(EmployeeNumber)
+		Salary=float(EAT(earningsafterinsurance(EBT)))
+		print('{}:{:.2f}'.format(EmployeeNumber,Salary))
 	
 if __name__=='__main__':
 	main()
