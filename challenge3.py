d744d4e1dc256f2909250c10db2077d719e09fc3
81623405 <81623405@qq.com>
2018-01-24 03:34:27 +0800

    challenge3

--- challenge3.py
+++ challenge3.py
@@ -5,12 +5,73 @@ import csv
 class Args(object):
 	def __init__(self):
 		self.args=sys.argv[1:]
-	#error
+		#a list
+	def _value_after_option(self,option):
+	#methods in a class usually start with an underline
+		try:
+			index=self.args.index(option)
+			return self.args.index(index+1)
+		except(ValueError, IndexError):
+			print('Parameter Error')
+			exit()
 
-#input: config.cfg, output:read config file and write it into config dict
+	@property
+	def config_path(self):
+		return self._value_after_option('-c')
+	#if we don't use property, when we use config_path, we have to write args.config_path(), with the bracket.
+
+	def userdata_path(self):
+		return self._value_after_option('-d')
+
+	def export_path(self):
+		return self._value_after_option('-o')
+
+args=Args()
+#instantiate args before we use it in the following class
+
+
+#input config.cfg, read it and write it into a config dict
 class Config(object):
 	def __init__(self):
 		self.config=self._read_config()
+		#self.config is now a dict in class Config
+	def _read_config(self):
+		config_path=args.config_path
+		config={}
+		with open(config_path) as f:
+			for line in f.readlines():
+				try:
+					key, value= line.split('=')
+					config[key.strip()]=float(value().split())
+					#make them a key and a value in dict config
+					except ValueError:
+					print('Parameter Error')
+		return config
+
+	def _get_config(self, key):
+		try:
+			return self.config[key]
+		except KeyError:
+			print('Config Error')
+			exit()
+
+	@property
+	def insurance_base_threshold(self):
+		return self._get_config('JishuL')
+	@property
+	def insurance_base_ceiling(self):
+	return self._get_config('JishuH')
+	@property
+	def insurance_rate(self):
+	return sum([
+		self._get_config('YangLao'),
+		self._get_config('YiLiao'),
+		self._get_config('ShiYe'),
+		self._get_config('GongShang'),
+		self._get_config('ShengYu'),
+		self._get_config('GongJiJin'),
+		])
+config=Config()
 
 #inpu: userdata file, output: userdata list
 class UserData(object):
@@ -24,6 +85,14 @@ class UserData(object):
 class IncomeTaxCalculator(object):
 	def __init__(self)
 
+
+if __name__=='__main__':
+
+
+
+
+
+#####################
 
 from collections import namedtuple
 
